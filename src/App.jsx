4938f6454014c9220a29ff3fe6d4a82e183f8df2
Juan Daniel Muñoz Queupul <munoz.queupul.daniel.juan@gmail.com>
2020-06-26 17:54:33 -0400

    58. Leer documentos de Firestore (parte 2)

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,17 @@ import { firebase } from './firebase'
 
 function App() {
 
+  const [tareas, setTareas] = React.useState([]);
+
   React.useEffect(() => {
 
     const obtenerDatos = async () => {
       const db = firebase.firestore();  
       try {
             const data = await db.collection('tareas').get();
-            console.log(data.docs);
+            const arrayTareas = data.docs.map(doc => ({id:doc.id, ...doc.data()}))
+            console.log(arrayTareas);
+            setTareas(arrayTareas);
         } catch (error) {
           console.log(error);
         }
@@ -22,6 +26,27 @@ function App() {
   return (
     <div className="container">
       <h1 className="text-center">Proyecto Crud Firestore</h1>
+        <div className="row">
+          <div className="col-md-6">
+            <ul className="list-group">
+              Lista de Tareas
+              {
+                tareas.map(item => (
+                  <li className="list-group-item" key={item.id}> 
+                    {item.name}
+                  </li>
+                ))
+              }
+
+            </ul>
+             
+          </div>
+          <div className="col-md-6">
+              Formulario
+          </div>
+        </div>
+
+
     </div>
   );
 }
