0274687da24eae6608dfc7c4ac332d413b595a0c
Juan Daniel Muñoz Queupul <jdannymq@gmail.com>
2020-06-26 15:40:24 -0400

    limpieza de codigo

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="text-center">Proyecto Crud Firestore</h1>
+    </div>
+  );
+}
+
+export default App;
